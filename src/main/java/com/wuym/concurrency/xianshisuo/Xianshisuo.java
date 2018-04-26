13ce32567bee2f0c3ae9ee11c9ff19082260a4fc
wuyumeng <275058342@qq.com>
2018-04-26 16:39:38 +0800

    读写锁

--- src/main/java/com/wuym/concurrency/xianshisuo/Xianshisuo.java
+++ src/main/java/com/wuym/concurrency/xianshisuo/Xianshisuo.java
@@ -1,7 +1,9 @@
 package com.wuym.concurrency.xianshisuo;
 
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * 显示锁
@@ -9,6 +11,14 @@ import java.util.concurrent.locks.ReentrantLock;
 public class Xianshisuo {
     private static Xianshisuo suo = new Xianshisuo();
     private final Lock lock = new ReentrantLock();
+
+    /**
+     * 读写锁
+     * */
+    private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final Lock readLock = rwLock.readLock();
+    private final Lock writeLock = rwLock.writeLock();
+
   // private final Lock lock = new ReentrantLock(true); 参数true为公平锁
     private int seq = 0;
     public int nextSeq(){
